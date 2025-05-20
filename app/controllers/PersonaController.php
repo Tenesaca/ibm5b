cf262d87edbb1723cc3516c0464bdf7799bb21fa
Tenesaca <125749199+Tenesaca@users.noreply.github.com>
2025-05-20 16:14:44 -0500

    Update PersonaController.php

--- app/controllers/PersonaController.php
+++ app/controllers/PersonaController.php
@@ -3,8 +3,8 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/apple5a/config/database.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/apple5a/app/models/Persona.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/ibm5b/config/database.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/ibm5b/app/models/Persona.php';
 
 class PersonaController {
     private $persona;

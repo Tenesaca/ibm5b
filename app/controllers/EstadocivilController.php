b68ac684ba91273ad4019151991a63ec9e7d1f0a
Tenesaca <125749199+Tenesaca@users.noreply.github.com>
2025-05-20 16:15:13 -0500

    Update EstadocivilController.php

--- app/controllers/EstadocivilController.php
+++ app/controllers/EstadocivilController.php
@@ -3,8 +3,8 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 // En estadocivilController.php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/apple5a/config/database.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/apple5a/app/models/Estadocivil.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/ibm5b/config/database.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/ibm5b/app/models/Estadocivil.php';
 
 class estadocivilController {
     private $estadocivil;
